1f5856fc5343baff984f37c6dea58c25b7910028
timur200362 <94241976+timur200362@users.noreply.github.com>
2022-04-25 22:31:43 +0300

    кк

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using Inf107_2_.Tree;
 using Inf107_2_.Delegate;
 using Inf107_2_.EventExample.CustomExample;
-using Inf107_2_.Reflection;
 
 namespace Inf107_2_
 {
@@ -12,8 +11,7 @@ namespace Inf107_2_
         {
             //CustomExampleRunner cesRunner = new CustomExampleRunner();
             //cesRunner.Run();
-            ReflectionRunner rr = new ReflectionRunner();
-            rr.Run();
+            
         }
     }
 }
